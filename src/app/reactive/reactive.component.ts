cdb6240ad7bec668e067c84259ff1383ebd985dd
Bruno Staub <bruno.staub@gmail.com>
2018-08-23 19:55:07 +0200

    reactive driven part1 bis 111 finished

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ReactiveComponent implements OnInit {
+  myForm: FormGroup;
 
   constructor() { }
 
+  onSubmit() {
+    console.log(this.myForm);
+  }
+
   ngOnInit() {
+    this.myForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'password': new FormControl(null),
+    });
   }
 
 }
